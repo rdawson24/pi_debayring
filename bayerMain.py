fc8e67bf1133dc92b0e5af3966f1c3c49d841474
rdawson24 <ryan.dawson@student.uts.edu.au>
2022-05-05 00:28:26 +1000

    Update bayerMain.py

--- bayerMain.py
+++ bayerMain.py
@@ -14,16 +14,17 @@ import matlab.engine
 def main():
     roi = ()
     img = Image.open(sys.argv[1])
-    roi = (80, 300, 100, 100)  # Region of Interest
+    roi = (4, 4, 4, 4)  # Region of Interest
 
     # data = PIL2array(img)
     data = np.array(img)[:, :, :3]
     data_crop = data[roi[1] : roi[1] + roi[3], roi[0] : roi[0] + roi[2]]
 
-    bt.npShow(data, "Original")
+    #bt.npShow(data, "Original")
+    bt.npSave(data_crop, "ref")
     #bt.npShow(data_crop, "Cropped Original", 8)
 
-    if len(roi) == 5:
+    if len(roi) == 4:
         ref = data_crop
     else:
         ref = data
@@ -32,20 +33,20 @@ def main():
     bayr = bayer.bayer(ref, bayerPhase)
     bayr_c = bayer.bayer_colour(ref, bayerPhase)
 
-    bt.npShow(bayr_c, "Bayered (false colour)", 8)
+    #bt.npShow(bayr_c, "Bayered (false colour)", 8)
     # b_img = bt.array2PIL(bayr, img.size)
 
     debay_img = debayerNN.debayer(bayr, bayerPhase).astype("uint8")
-    bt.npShow(debay_img, "Nearest Neighbour Debayered", 8)
+    #bt.npShow(debay_img, "Nearest Neighbour Debayered", 8)
     bt.npSave(debay_img, "Output")
 
     diff = (ref.astype("int16") - debay_img.astype("int16")) + 127
     np.clip(diff, 0, 255, out=diff)
     diff_img = diff.astype("uint8")
-    bt.npShow(diff_img, "Difference", 8)
+    #bt.npShow(diff_img, "Difference", 8)
 
     eng = matlab.engine.start_matlab()
-    psnrResult = eng.PSNR(sys.argv[1])
+    psnrResult = eng.PSNR()
     print("PSNR:", psnrResult)
 
     #print("PSNR R =", metrics.psnr(ref[0], diff_img[0]))
